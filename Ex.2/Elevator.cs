124ecf02216c26ceaee94c8912e0e1db4e3a45f5
Ruslan <Ruslan@LAPTOP-C5MDF2BQ>
2021-03-26 12:32:47 +0600

    Добавьте файлы проекта.

---
+++ Ex.2/Elevator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace Ex._2
+{
+    class Elevator
+    {
+        private int elev_lev;
+        public int ElevLevel
+        {
+            get { return elev_lev; }
+            set { elev_lev = value; }
+        }
+        public int max_pass_number;
+
+        public Elevator()
+        {
+            ElevLevel = elev_lev;
+            max_pass_number = 8;
+        }
+
+        public void GoesToPassenger(int passLevel)
+        {
+            int diff = 0;
+            if ((ElevLevel - passLevel) > 0)
+            {
+                diff = ElevLevel - passLevel;
+            }
+            if ((ElevLevel - passLevel) < 0)
+            {
+                diff = (ElevLevel - passLevel) * (-1);
+            }
+            for (int i = 1; i <= diff; i++)
+            {
+                Console.WriteLine($"В пути... {i} этаж");
+                Thread.Sleep(1000);
+                ElevLevel = i;
+            }
+        }
+
+        public void GoesUp(int dirLevel)
+        {
+            for (int i = (ElevLevel + 1); i <= dirLevel; i++)
+            {
+                Console.WriteLine($"Поднимаюсь на {i} этаж");
+                Thread.Sleep(1000);
+            }
+            ElevLevel = dirLevel; 
+        }
+
+        public void GoesDown(int dirLevel)
+        {
+            for (int i = ElevLevel; i >= dirLevel; i--)
+            {
+                Console.WriteLine($"Опускаюсь на {i} этаж");
+                Thread.Sleep(1000);
+            }
+            ElevLevel = dirLevel;
+        }
+
+        public static void OpenDoor()
+        {
+            Console.WriteLine("Двери открываются");
+        }
+       
+        public static void CloseDoor()
+        {
+            Console.WriteLine("Двери закрываются");
+        }
+    }
+}
---
+++ Ex.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+namespace Ex._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int curr_etaj, dir_etaj, passengersNumber;
+            Elevator elev = new();
+            Console.WriteLine("В здании 12 этажей.");
+       
+        a: Console.WriteLine("Текущий этаж: ");
+            curr_etaj = Convert.ToInt32(Console.ReadLine());
+            if ((curr_etaj > 12) || (curr_etaj < 1))
+            {
+                Console.WriteLine("Ошибка. Попробуйте еще раз");
+                goto a;
+            }
+            elev.GoesToPassenger(curr_etaj);
+            Console.WriteLine($"Лифт прибыл на {elev.ElevLevel} этаж" + $"");
+            Thread.Sleep(1000);
+            Elevator.OpenDoor();
+
+        b: Console.WriteLine("Сколько вас: ");
+            passengersNumber = Convert.ToInt32(Console.ReadLine());
+            if (passengersNumber > elev.max_pass_number)
+            {
+                Console.WriteLine("Перегрузка, уменьшите количество пассажиров");
+                Thread.Sleep(1000);
+                goto b;
+            }
+       
+        c: Console.WriteLine("На какой этаж:");
+            dir_etaj = Convert.ToInt32(Console.ReadLine());
+            Thread.Sleep(1200);
+            Elevator.CloseDoor();
+            Thread.Sleep(1200);
+            if (dir_etaj > curr_etaj)
+            {
+                elev.GoesUp(dir_etaj);
+                Console.WriteLine($"Лифт на {elev.ElevLevel} этаже");
+                Thread.Sleep(1000);
+                Elevator.OpenDoor();
+                Thread.Sleep(3000);
+                Elevator.CloseDoor();
+            }
+            else if (dir_etaj < curr_etaj)
+            {
+                elev.GoesDown(dir_etaj);
+                Console.WriteLine($"Лифт на {elev.ElevLevel} этаже");
+                Thread.Sleep(1400);
+                Elevator.OpenDoor();
+                Thread.Sleep(3000);
+                Elevator.CloseDoor();
+            }
+            else
+            {
+                Console.WriteLine("Выберите этаж");
+                Thread.Sleep(1000);
+                goto c;
+            }
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
